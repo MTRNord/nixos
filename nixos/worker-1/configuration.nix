5cc7ccb337c4f79377f8921526ef6a17e5a21382
MTRNord <mtrnord1@gmail.com>
2023-08-09 15:17:33 +0200

    Update the module name

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -9,7 +9,7 @@
 
     # Or modules from other flakes (such as nixos-hardware):
     # inputs.hardware.nixosModules.common-cpu-amd
-    inputs.hardware.nixosModules.common-ssd
+    inputs.hardware.nixosModules.common-pc-ssd
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
