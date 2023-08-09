655a75110704810309fea11619bf456a77aa41bd
MTRNord <mtrnord1@gmail.com>
2023-08-09 16:27:06 +0200

    Only use age

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -164,6 +164,7 @@
 
   # SOPS
   sops.age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
+  sops.gnupg.sshKeyPaths = [];
   # This is using an age key that is expected to already be in the filesystem
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   # This will generate a new key if the key specified above does not exist
