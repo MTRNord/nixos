92eef93db8c730ef7a4fc175839c8c9d3c63abec
MTRNord <mtrnord1@gmail.com>
2023-08-09 16:25:54 +0200

    Ensure order of operation

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -150,6 +150,18 @@
   };
 
 
+  # This setups a SSH server. Very important if you're setting up a headless system.
+  # Feel free to remove if you don't need it.
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Forbid root login through SSH.
+      PermitRootLogin = "no";
+      # Use keys only. Remove if you want to SSH using password (not recommended)
+      PasswordAuthentication = false;
+    };
+  };
+
   # SOPS
   sops.age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
   # This is using an age key that is expected to already be in the filesystem
@@ -198,18 +210,6 @@
   };
 
   users.users."root".passwordFile = config.sops.secrets.root_initial_password.path;
-
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Use keys only. Remove if you want to SSH using password (not recommended)
-      PasswordAuthentication = false;
-    };
-  };
 
   # Darling Erasure
   environment.etc = {
