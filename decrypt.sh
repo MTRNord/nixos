287c8c20c5568ebeeeba21d3d89d1b1cda4f092c
MTRNord <mtrnord1@gmail.com>
2023-08-09 15:24:43 +0200

    reencrypt with correct key

--- decrypt.sh
+++ decrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export SOPS_AGE_KEY_FILE="$(pwd)/age.agekey"
-sops --ignore-mac --age=age1esjyg2qfy49awv0ptkzvpk425adczjr38m37w2mmcahzc4p8n54sll2nzh --decrypt --in-place "$1"
+sops --ignore-mac --decrypt --in-place "$1"
