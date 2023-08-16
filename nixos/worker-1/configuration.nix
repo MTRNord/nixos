747bdcd8c737cdf06fca9f8e74ede6c43140fd61
MTRNord <mtrnord1@gmail.com>
2023-08-16 02:37:53 +0200

    Fix sounds

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -475,7 +475,7 @@
         [tests]
         exten => 100,1,Answer()
         same => n,Wait(1)
-        same => n,Playback(/var/lib/asterisk/sounds/en/hello-world.gsm)
+        same => n,Playback(/var/lib/asterisk/sounds/en/hello-world)
         same => n,Hangup()
 
         [epvpn]
@@ -488,7 +488,7 @@
 
         [externals]
         exten => 2903,1,Answer()
-        same => n,Playback(/var/lib/asterisk/sounds/en/hello-world.gsm)
+        same => n,Playback(/var/lib/asterisk/sounds/en/hello-world)
         same => n,WaitExten(30)
         same => n,Hangup()
 
