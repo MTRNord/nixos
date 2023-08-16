e898b0bf30a889487ec2c92cde846077f417a61e
MTRNord <mtrnord1@gmail.com>
2023-08-16 16:41:58 +0200

    meow

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -489,8 +489,8 @@
         exten => _0199XXXX,1,Answer()
         same => n,Verbose(0, Going to play hello)
         same => n,Playback(/var/lib/asterisk/sounds/en/hello-world)
-        same => n,Verbose(0, Going to set CALLERID)
-        same => n,Set(CALLERID(num)=2903)
+        ;same => n,Verbose(0, Going to set CALLERID)
+        ;same => n,Set(CALLERID(num)=2903)
         same => n,Verbose(0, Going to dial ''${EXTEN:4}@eventphone)
         same => n,Dial(PJSIP/''${EXTEN:4}@eventphone)
 
