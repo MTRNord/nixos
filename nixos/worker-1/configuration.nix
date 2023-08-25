06c87319ecaeb677add7e3ec1d1f38c31ca80bc0
MTRNord <mtrnord1@gmail.com>
2023-08-25 12:16:23 +0200

    Fix typo

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -466,7 +466,7 @@
       initialClusterState = "existing";
       listenClientUrls = [ "http://100.64.0.1:2379" ];
       listenPeerUrls = [ "http://100.64.0.1:2380" ];
-      initalCluster = [ "nordgedanken=http://100.64.0.3:2380" ];
+      initialCluster = [ "nordgedanken=http://100.64.0.3:2380" ];
       extraConf = {
         "UNSUPPORTED_ARCH" = "arm64";
       };
