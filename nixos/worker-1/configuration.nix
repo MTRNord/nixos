14a99a9160756366d11fce7449a00985f704d8b4
MTRNord <mtrnord1@gmail.com>
2023-08-25 14:20:44 +0200

    make pg also listen to localhost

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -484,7 +484,7 @@
       postgresqlPackage = pkgs.postgresql_14;
 
       settings = {
-        postgresql.listen = "100.64.0.1:5432";
+        postgresql.listen = "127.0.0.1,100.64.0.1:5432";
         etcd = {
           hosts = [
             "100.64.0.3:2379"
