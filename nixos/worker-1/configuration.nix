944750acc611e3eddf127b68f27e7904e54235c3
MTRNord <mtrnord1@gmail.com>
2023-08-25 12:38:39 +0200

    use correct settings

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -463,11 +463,12 @@
 
     etcd = {
       enable = true;
+      initialClusterState = "existing";
       listenClientUrls = [ "http://100.64.0.1:2379" ];
       listenPeerUrls = [ "http://100.64.0.1:2380" ];
       initialCluster = [
-        "nordgedanken=http://100.64.0.3:2380"
         "worker-1=http://100.64.0.1:2380"
+        "nordgedanken=http://100.64.0.3:2380"
       ];
       extraConf = {
         "UNSUPPORTED_ARCH" = "arm64";
