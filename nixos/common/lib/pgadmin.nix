cf8cf6a296f2e949cdd7245dae42b9340932b39d
MTRNord <mtrnord1@gmail.com>
2023-09-04 20:30:25 +0200

    Activate pgadmin

--- nixos/common/lib/pgadmin.nix
+++ nixos/common/lib/pgadmin.nix
@@ -1,11 +1,27 @@
 { lib, pkgs, config, ... }:
 {
-  services.pgadmin = {
-    enable = true;
-    settings = {
-      DEFAULT_SERVER = "100.64.0.1";
+  services = {
+    postgresql = {
+      enableJIT = true;
+      enable = false;
+      enableTCPIP = true;
+      settings = {
+        listen_addresses = "100.64.0.1";
+      };
+      authentication = ''
+        host    all             all             100.64.0.0/10           md5
+        host    replication     all             100.64.0.0/10           md5
+        host    all             all             10.100.12.1/32          md5
+        host    replication     all             10.100.12.1/32          md5
+      '';
     };
-    initialEmail = "mtrnord@nordgedanken.dev";
-    initialPasswordFile = config.sops.secrets.pgadmin_password.path;
+    pgadmin = {
+      enable = true;
+      settings = {
+        DEFAULT_SERVER = "100.64.0.1";
+      };
+      initialEmail = "mtrnord@nordgedanken.dev";
+      initialPasswordFile = config.sops.secrets.pgadmin_password.path;
+    };
   };
 }
