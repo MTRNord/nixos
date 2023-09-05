db64b34936b42c24e89dd841c82fa74593d9c94b
MTRNord <mtrnord1@gmail.com>
2023-09-05 16:29:31 +0200

    Fix admin section

--- nixos/common/lib/envoy.nix
+++ nixos/common/lib/envoy.nix
@@ -4,17 +4,17 @@
     envoy = {
       enable = true;
       settings = {
-        static_resources = {
-          admin = {
-            access_log_path = "/dev/null";
-            address = {
-              socket_address = {
-                protocol = "TCP";
-                address = "127.0.0.1";
-                port_value = "9901";
-              };
+        admin = {
+          access_log_path = "/dev/null";
+          address = {
+            socket_address = {
+              protocol = "TCP";
+              address = "127.0.0.1";
+              port_value = "9901";
             };
           };
+        };
+        static_resources = {
           listeners = [
             {
               name = "postgres";
