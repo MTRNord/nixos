8e82c2922bbd62839fb0c9b5b1536f8029cfb87f
MTRNord <mtrnord1@gmail.com>
2023-09-08 17:52:00 +0200

    Fix pgbouncer

--- overlays/default.nix
+++ overlays/default.nix
@@ -35,7 +35,7 @@
         submodules = true;
       };
 
-      nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.autoreconfHook269 ];
+      nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.autoreconfHook264 ];
     });
   };
 
