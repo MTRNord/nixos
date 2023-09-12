5a4b6c80addc2ff55f5d68e0c94354e91df6636d
MTRNord <mtrnord1@gmail.com>
2023-09-12 21:05:28 +0200

    meow

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -132,6 +132,14 @@
         routes = [
           { routeConfig.Gateway = "fe80::1"; }
           { routeConfig = { Gateway = "172.31.1.1"; GatewayOnLink = true; }; }
+
+          # prevent some local traffic Hetzner doesn't like
+          { routeConfig = { Destination = "172.16.0.0/12"; Type = "unreachable"; }; }
+          { routeConfig = { Destination = "192.168.0.0/16"; Type = "unreachable"; }; }
+
+          # { routeConfig = { Destination = "10.0.0.0/8"; Type = "unreachable"; }; }
+          { routeConfig = { Destination = "fc00::/7"; Type = "unreachable"; }; }
+
         ];
       };
     };
