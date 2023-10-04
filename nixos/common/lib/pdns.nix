2b4618af5155a37e386d9f174a804746e1be40ae
MTRNord <mtrnord1@gmail.com>
2023-10-04 17:44:54 +0200

    Fix listener

--- nixos/common/lib/pdns.nix
+++ nixos/common/lib/pdns.nix
@@ -2,15 +2,15 @@
 {
   services.powerdns = {
     enable = true;
-    extraConfig = ''
-      launch=gsqlite3
-      master=yes
-      webserver-address=0.0.0.0
-      webserver-allow-from=127.0.0.1,::1,10.244.0.0/16,31.17.243.193
-      webserver-port=8081
-      gsqlite3-database=/var/lib/pdns/pdns.db
-    '';
   };
+  pdns.config = ''
+    launch=gsqlite3
+    master=yes
+    webserver-address=0.0.0.0
+    webserver-allow-from=127.0.0.1,::1,10.244.0.0/16,31.17.243.193
+    webserver-port=8081
+    gsqlite3-database=/var/lib/pdns/pdns.db
+  '';
 
   networking.firewall.allowedTCPPorts = [ 8081 ];
 }
--- nixos/worker-2/configuration.nix
+++ nixos/worker-2/configuration.nix
@@ -122,7 +122,7 @@
     };
   };
 
-  services.powerdns.extraConfig = config.services.powerdns.extraConfig + ''
+  services.powerdns.extraConfig = pdns.config + ''
     local-address=37.27.5.79 2a01:4f9:c012:54d3::
   '';
 
