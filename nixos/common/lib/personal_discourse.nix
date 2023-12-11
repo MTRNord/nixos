24d0bc420104106a79001055fdce0f49910c7ecc
MTRNord <mtrnord1@gmail.com>
2023-12-11 12:09:26 +0100

    meow

--- nixos/common/lib/personal_discourse.nix
+++ nixos/common/lib/personal_discourse.nix
@@ -30,7 +30,7 @@
       contactEmailAddress = "support@miki.community";
     };
     redis = {
-      host = "localhost";
+      host = "127.0.0.1";
     };
     hostname = "personal.midnightthoughts.space";
     plugins = with config.services.discourse.package.plugins; [
