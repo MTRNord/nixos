6fddf1dd64962d66e3352e966a2ff26c22c6bf67
MTRNord <mtrnord1@gmail.com>
2024-01-27 18:38:26 +0100

    Meow

--- nixos/worker-1/configuration.nix
+++ nixos/worker-1/configuration.nix
@@ -467,6 +467,7 @@ in {
         protocol bgp worker2 {
           local 10.0.2.1 as 64513;        # Use a private AS number
           neighbor 10.0.2.2 as 64514;    # Our neighbor ...
+          direct;
           #multihop;                            # ... which is connected indirectly
           ipv4 {
             export filter allowed_ips;
@@ -485,6 +486,7 @@ in {
         protocol bgp nordgedanken {
           local 10.0.2.1 as 64513;        # Use a private AS number
           neighbor 10.0.1.2 as 64512;    # Our neighbor ...
+          direct;
           #multihop;                            # ... which is connected indirectly
           ipv4 {
             export filter allowed_ips;
--- nixos/worker-2/configuration.nix
+++ nixos/worker-2/configuration.nix
@@ -567,6 +567,7 @@ in {
         protocol bgp worker1 {
           local 10.0.2.2 as 64514;        # Use a private AS number
           neighbor 10.0.2.1 as 64513;    # Our neighbor ...
+          direct;
           #multihop;                            # ... which is connected indirectly
           ipv4 {
             export filter allowed_ips;
@@ -585,6 +586,7 @@ in {
         protocol bgp nordgedanken {
           local 10.0.2.2 as 64514;        # Use a private AS number
           neighbor 10.0.1.2 as 64512;    # Our neighbor ...
+          direct;
           #multihop;                            # ... which is connected indirectly
           ipv4 {
             export filter allowed_ips;
