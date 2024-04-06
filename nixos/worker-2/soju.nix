c9ceaad9de6bea7b9346fc048581deae81ac41de
MTRNord <mtrnord1@gmail.com>
2024-04-06 21:00:11 +0200

    Set missing cert paths

--- nixos/worker-2/soju.nix
+++ nixos/worker-2/soju.nix
@@ -7,12 +7,15 @@
   environment.persistence."/persist" = {
     directories = [
       "/var/lib/soju"
+      "/var/lib/acme"
     ];
   };
   services.soju = {
     enable = true;
     hostName = "soju.midnightthoughts.space";
     enableMessageLogging = true;
+    tlsCertificateKey = "/var/lib/acme/soju.midnightthoughts.space/key.pem";
+    tlsCertificate = "/var/lib/acme/soju.midnightthoughts.space/fullchain.pem";
   };
 
   security.acme.certs."soju.midnightthoughts.space" = {
